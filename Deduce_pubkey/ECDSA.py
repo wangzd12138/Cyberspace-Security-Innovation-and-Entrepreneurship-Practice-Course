30c74772097791b91e688bfd2350a43808332842
wzd12138 <wangzhidonga23187@163.com>
2022-07-26 18:06:34 +0800

    Deduce public key from signature with ECDSA(Update)

--- Deduce_pubkey/ECDSA.py
+++ Deduce_pubkey/ECDSA.py
@@ -1,5 +1,63 @@
 import secrets
 from hashlib import sha256
+import gmpy2
+
+
+
+
+# 有限域
+P = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF
+# 椭圆曲线
+N = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
+A = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC
+B = 0x28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93
+
+G_X = 0x32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7
+G_Y = 0xBC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0
+
+G = (G_X, G_Y)
+
+
+
+def elliptic_add(p, q):
+    if p == 0 and q == 0: return 0
+    elif p == 0: return q
+    elif q == 0: return p
+    else:
+        return _extracted_from_elliptic_add_7(p, q)
+
+
+
+def _extracted_from_elliptic_add_7(p, q):
+    if p[0] > q[0]:
+        p, q = q, p
+    slope = (q[1] - p[1])*gmpy2.invert(q[0] - p[0], P) % P
+
+    r = [(slope**2 - p[0] - q[0]) % P]
+    r.append((slope*(p[0] - r[0]) - p[1]) % P)
+
+    return (r[0], r[1])
+
+
+def elliptic_double(p):
+    slope = (3*p[0]**2 + A)*gmpy2.invert(2*p[1], P) % P
+
+    r = [(slope**2 - 2*p[0]) % P]
+    r.append((slope*(p[0] - r[0]) - p[1])%P)
+
+    return (r[0], r[1])
+
+def elliptic_multiply(s, p):
+    n = p
+    r = 0 
+    s_binary = bin(s)[2:] 
+    s_length = len(s_binary)
+    for i in reversed(range(s_length)):
+        if s_binary[i] == '1':
+            r = elliptic_add(r, n)
+        n = elliptic_double(n)
+
+    return r
 
 def Legendre(y,p): 
     return pow(y,(p - 1) // 2,p)
@@ -34,95 +92,7 @@ def Tonelli_Shanks(y,p):
                 i = 0 
     return r
 
-def extended_euclidean(j, k):
-    if j == k:
-        return (j, 1, 0)
-    i = 0
-    j_array = [j]
-    k_array = [k]
-    q_array = []
-    r_array = []
 
-    prev_r_tag = False
-
-    while not (prev_r_tag):
-        q_array.append(k_array[i]//j_array[i])
-        r_array.append(k_array[i]%j_array[i])
-        k_array.append(j_array[i])
-        j_array.append(r_array[i])
-        i += 1
-        if r_array[i-1] == 0:
-            prev_r_tag = True
-    i -= 1
-    gcd = j_array[i]
-    x_array = [1]
-    y_array = [0]
-    i -= 1
-    steps = i
-    while i >= 0:
-        y_array.append(x_array[steps-i])
-        x_array.append(y_array[steps-i] - q_array[i]*x_array[steps-i])
-        i -= 1
-
-    return (gcd, x_array[-1], y_array[-1])
-
-def mod_inverse(j, n):
-    (gcd, x, y) = extended_euclidean(j, n)
-    return x%n if gcd == 1 else -1
-
-
-# 有限域
-P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
-# 椭圆曲线
-N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
-A = 0
-B = 7
-
-
-def elliptic_add(p, q):
-    if p == 0 and q == 0: return 0
-    elif p == 0: return q
-    elif q == 0: return p
-    else:
-        return _extracted_from_elliptic_add_7(p, q)
-
-
-def _extracted_from_elliptic_add_7(p, q):
-    if p[0] > q[0]:
-        p, q = q, p
-    slope = (q[1] - p[1])*mod_inverse(q[0] - p[0], P) % P
-
-    r = [(slope**2 - p[0] - q[0]) % P]
-    r.append((slope*(p[0] - r[0]) - p[1]) % P)
-
-    return (r[0], r[1])
-
-
-def elliptic_double(p):
-    slope = (3*p[0]**2 + A)*mod_inverse(2*p[1], P) % P
-
-    r = [(slope**2 - 2*p[0]) % P]
-    r.append((slope*(p[0] - r[0]) - p[1])%P)
-
-    return (r[0], r[1])
-
-def elliptic_multiply(s, p):
-    n = p
-    r = 0 
-    s_binary = bin(s)[2:] 
-    s_length = len(s_binary)
-    for i in reversed(range(s_length)):
-        if s_binary[i] == '1':
-            r = elliptic_add(r, n)
-        n = elliptic_double(n)
-
-    return r
-
-
-G_X = 55066263022277343669578718895168534326250603453777594175500187360389116729240
-G_Y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
-
-G = (G_X, G_Y)
 
 def generate_key():
     """生成公钥私钥
@@ -166,7 +136,7 @@ def Signature(private_key, message):
     k = secrets.randbelow(P)
     random_point = elliptic_multiply(k, G)
     r = random_point[0] % P
-    s = mod_inverse(k, N) * (e + r*private_key) % N
+    s = gmpy2.invert(k, N) * (e + r*private_key) % N
     return (r, s)
 
 
@@ -196,11 +166,11 @@ def deduce_pubkey(signature,message):
     eG=elliptic_multiply(e%N,G)
     negeG=(eG[0],P-eG[1])
     skGeG=elliptic_add(skG,negeG)
-    deduce_key1=elliptic_multiply(mod_inverse(r,N),skGeG)
+    deduce_key1=elliptic_multiply(gmpy2.invert(r,N),skGeG)
 
     skG=elliptic_multiply(s%N,point_2)
     skGeG=elliptic_add(skG,negeG)
-    deduce_key2=elliptic_multiply(mod_inverse(r,N),skGeG)
+    deduce_key2=elliptic_multiply(gmpy2.invert(r,N),skGeG)
     return deduce_key1,deduce_key2
 
 if __name__ == "__main__":
